1986ace4b419e07c3701765d39233917770c0928
neryuuk <966712+neryuuk@users.noreply.github.com>
2025-02-03 16:03:06 -0300

    versions

--- serve.sh
+++ serve.sh
@@ -1,8 +1,9 @@
 rm -rf _site .jekyll-cache Gemfile.lock;
 
-ruby -v;
-gem -v;
-bundle -v;
+echo "ruby $(ruby -v | egrep -o "[0-9]+\.[0-9]+\.[0-9]+")";
+echo "gem $(gem -v | egrep -o "[0-9]+\.[0-9]+\.[0-9]+")";
+echo "bundle $(bundle -v | egrep -o "[0-9]+\.[0-9]+\.[0-9]+")";
+echo "jekyll $(jekyll -v | egrep -o "[0-9]+\.[0-9]+\.[0-9]+")";
 
 bundle install;
 JEKYLL_ENV=production bundle exec jekyll serve --disable-disk-cache --future --livereload --safe --trace;
