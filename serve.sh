93a17c2ac5ca3ad1664d9d7f87061445f3d3f1bb
neryuuk <966712+neryuuk@users.noreply.github.com>
2025-02-18 10:59:56 -0300

    formatting

--- serve.sh
+++ serve.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
 source vars.sh
 
 rm -rf _site .jekyll-cache Dockerfile Gemfile.lock;
 
 sed "s/V_RUBY/$V_RUBY/;s/V_GEMS/$V_GEMS/;s/V_BUNDLER/$V_BUNDLER/" serve.dockerfile > Dockerfile;
 
-docker build . --no-cache --tag $TAG;
+docker build . \
+  --no-cache \
+  --tag $TAG;
 
 docker run --detach \
   --env JEKYLL_ENV=production \
